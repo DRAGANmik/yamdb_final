bf4f680d3e2da5b6c69dd944fcb91e987ccaa471
Mike <mike94.0653@ya.ru>
2021-07-21 01:23:22 +0300

    fix settings

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "yoursecretkey")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [""]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
